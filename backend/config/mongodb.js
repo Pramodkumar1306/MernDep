f0cac8e5b0e64a1c08276fe5bbc7b23a498c7baf
Pramodkumar1306 <pramod73kumar7415@gmail.com>
2025-06-06 20:07:50 +0530

    added url in file it self

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,8 +3,8 @@ import mongoos from 'mongoose'
 const connectDb = async() => {
     
     try{
-        await mongoos.connect(`mongodb+srv://varun73patil:varunchutiya@cluster0.mk5dl7k.mongodb.net/newData`);
-        console.log(' MongoDB connected successfully')
+        await mongoos.connect('mongodb+srv://varun73patil:varunchutiya@cluster0.mk5dl7k.mongodb.net/newData');
+        console.log('MongoDB connected successfully')
     }catch(error){
         console.log('Mongo Connection Db Error'+error)
     }
