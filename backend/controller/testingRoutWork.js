bff2816237687a0ef1f06827e9fc999d60c165d9
Pramodkumar1306 <pramod73kumar7415@gmail.com>
2025-06-06 19:11:43 +0530

    added the data

--- backend/controller/testingRoutWork.js
+++ backend/controller/testingRoutWork.js
@@ -1,7 +1,5 @@
-const testingNormal = (req,res) => {
-    res.send("Api sadasdWorking");
-    console.log("asdasdworking");
-    // res.json({success:true,messag:"Data is reaching"})
+const testingNormal = (req, res) => {
+    res.json({ success: true, messag: "Data is reaching" });
 }
 
 export  {testingNormal};
