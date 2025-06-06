783ddede3c651a3f90b80c6a764c44ecf0a18bba
Pramodkumar1306 <pramod73kumar7415@gmail.com>
2025-06-06 19:26:26 +0530

    added the mongoo

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,9 @@
 import mongoos from 'mongoose'
 
 const connectDb = async() => {
-
-    const mongoUri = process.env.MONGODB_URI;
-    
     
     try{
-        await mongoos.connect(`${mongoUri}/newData`);
+        await mongoos.connect(`mongodb+srv://varun73patil:varunchutiya@cluster0.mk5dl7k.mongodb.net/newData`);
         console.log(' MongoDB connected successfully')
     }catch(error){
         console.log('Mongo Connection Db Error'+error)
